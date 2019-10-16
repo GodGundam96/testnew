848feec8929823f7b1d62ff2a6caf6c2be592a44
Puwaniti <Puwaniti@gmail.com>
2019-10-17 00:27:58 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+from pprint import  pprint
+print('Hello world !!')
+print('Why i\'am here')
+
+def say_word():
+    name = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+    for i in name:
+        print(i)
+
+say_word()
+dict = [{'name':{'surname':'puwaniti','nickname':'god'},'age':22}]
+
+pprint(dict)
